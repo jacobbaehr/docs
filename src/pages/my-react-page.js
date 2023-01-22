1a66b710f75a477045e70f5d457e34b9a6a10e44
Jacob Baehr <73747007+jacobbaehr@users.noreply.github.com>
2023-01-21 20:14:47 -0600

    initial commit

---
+++ src/pages/my-react-page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "@theme/Layout";
+
+export default function MyReactPage() {
+    return (
+        <Layout>
+            <h1>My React Page</h1>
+            <p>this is a react page</p>
+        </Layout>
+    );
+}
+
